b543e7f8ec368873332d89883f345c5d11d360e8
Kevin Capucci <patrickzhipz@gmail.com>
2023-10-26 15:39:52 -0400

    add grid and new features

--- hompage.js
+++ hompage.js
@@ -3,39 +3,81 @@
 const container = document.querySelector(".container");
 const div = document.createElement("div");
 div.className = "div";
+container.appendChild(div);
 
 
 
 
-function grid (r) {
+function grid (r, color) {
+    const containerSize = 500; // Adjust this to your preferred container size
+            const gridSize = `${containerSize / r}px`;
     for (let i = 0; i < r; i++) {
-        // const Rowdiv = document.createElement('div');
-        // const div = document.createElement("div");
-        // div.className = "div";
         const Rowdiv = document.createElement("div");
         Rowdiv.appendChild(document.createTextNode(""));
         Rowdiv.className = "row";
-        // for (let j=0; j < c; j++) {
-            // const div = document.createElement("div");
-            // div.className = "div";
-            for (let j=0; j < r; j++) {
+
+    
+         for (let j=0; j < r; j++) {
+            
             const Columndiv = document.createElement("div");  
+            Columndiv.style.width = gridSize;
+            Columndiv.style.height = gridSize;
             Columndiv.className = "column";   
             Columndiv.appendChild(document.createTextNode(""));
             Rowdiv.appendChild(Columndiv); 
+            Columndiv.addEventListener('mouseover', () => {
+                Columndiv.style.background = color;
+            })
+            Columndiv.addEventListener('mouseout', () => {
+                Columndiv.style.background = "";
+            })
+        
             }
-     
-            
-        // }  
+    
         
-        div.appendChild(Rowdiv);
-        container.appendChild(div);
-    }
+            div.appendChild(Rowdiv);
+           
        
-        
+    
+    }
+    
 }
 
-grid(16);
+grid(16, "white");
+
+
+
+
+function changeGrid () {
+    let choices = prompt("Change the # of squares on each side, enter an integer between 1-100");
+    
+    if(choices <= 100) {
+
+        div.innerHTML = "";
+        grid(choices);
+
+    
+    } else {
+        alert("Invalid, you must enter an integer between 1-100");
+        div.innerHTML = "";
+        grid(16,16);
+    }
+
+}
+
+
+const button = document.querySelector(".button");
+button.addEventListener("click", changeGrid);
+
+
+const rainbow = document.querySelector(".Rainbow");
+rainbow.addEventListener("click", )
+
+
+
+
+
+
 
 
 
